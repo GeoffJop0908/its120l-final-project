49b120f8660ccbfdbf72beac16faafbda1144ed0
GeoffJop0908 <geoffjop0908@gmail.com>
2025-03-03 23:38:37 +0800

    remove react chatbotify

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import Home from './pages/Home.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />, // Wrap everything in RootLayout
+    element: <Home />,
   },
 ]);
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import '../App.css';
+
+export default function Home() {
+  return (
+    <div className="bg-neutral-800 h-[100vh] flex items-center justify-center text-white flex-col">
+      <h1 className="text-5xl">Hi</h1>
+    </div>
+  );
+}
